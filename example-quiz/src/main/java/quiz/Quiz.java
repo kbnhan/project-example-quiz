d9ffc71042cd95f2d2c1f67ccd070b688d3eb0e4
Kevin Nhan <kbnhan@users.noreply.github.com>
2019-06-19 15:30:55 -0400

    Added initial files

---
+++ example-quiz/src/main/java/quiz/Quiz.java
@@ -0,0 +1,19 @@
+package quiz;
+
+public class Quiz {
+
+    private String answer = "Blue";
+    private String user_answer;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setUserAnswer(String user_answer) {
+        this.user_answer = user.answer;
+    }
+
+    public String getUserAnswer() {
+        return user_answer;
+    }
+}
---
+++ example-quiz/src/main/java/quiz/QuizController.java
@@ -0,0 +1,23 @@
+package quiz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class QuizController {
+
+    @GetMapping("/quiz")
+    public String quizForm(Model model) {
+        model.addAttribute("quiz", new Quiz());
+        return "quiz";
+    }
+
+    @PostMapping("/result")
+    public String quizSubmit(@ModelAttribute Quiz quiz) {
+        return "result";
+    }
+
+}
